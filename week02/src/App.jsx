071552c0c7f4fcf1a2f0150ecbe1befa726db791
yeseul0000 <fiddich999@gmail.com>
2024-02-10 21:20:19 +0900

    css적용 외 작업 완료

--- week02/src/App.jsx
+++ week02/src/App.jsx
@@ -1,23 +1,81 @@
-import { useState } from "react";
-import { ContentCard } from "./components/ContentCard"
+import { useState, useEffect } from "react";
 import "./App.css";
+import "./main.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://yts.mx/api/v2/list_movies.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const fetchedMovies = data.data.movies;
+        console.log(fetchedMovies);
+        setMovies(fetchedMovies);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const MovieSelect = () => (
+    <select name="selectedMovie">
+      <option value="xx">Select a movie</option>
+      {movies.map((movie) => (
+        <option key={movie.id}>{movie.title}</option>
+      ))}
+    </select>
+  );
+
+  const Stars = () => (
+    <select name="selectedStars">
+      <option value="xx">별점 등록</option>
+      <option value="⭐️">⭐️</option>
+      <option value="⭐️⭐️">⭐️⭐️</option>
+      <option value="⭐️⭐️⭐️">⭐️⭐️⭐️</option>
+      <option value="⭐️⭐️⭐️⭐️">⭐️⭐️⭐️⭐️</option>
+      <option value="⭐️⭐️⭐️⭐️⭐️">⭐️⭐️⭐️⭐️⭐️</option>
+    </select>
+  );
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const selectedMovie = formData.get("selectedMovie");
+    const selectedStars = formData.get("selectedStars");
+    const comment = formData.get("comment");
+
+    if (selectedMovie === "xx" || selectedStars === "xx") return;
+
+    const selectedMovieObject = movies.find(
+      (movie) => movie.title === selectedMovie
+    );
+
+    const imgUrl = selectedMovieObject
+      ? selectedMovieObject.medium_cover_image
+      : "";
+
+    const form = document.querySelector(".form");
+    const newElementsHTML = `
+    <div>
+      <img src=${imgUrl} >
+      <h2>${selectedMovie}</h2>
+      <p>${selectedStars}</p>
+      <p>${comment}</p>
+    </div>
+  `;
+
+    form.insertAdjacentHTML("beforebegin", newElementsHTML);
+  };
 
   return (
-    <ul>
-      {items.map(({id}) => {
-        const photoUrl; //포켓호스트
-        const photoAlt;
-
-        return (
-          <li key={id}>
-            <ContentCard photo = {photoUrl} name = {photoAlt}/>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="">
+      <h1>영화 평점 매기기</h1>
+      <form className="form" onSubmit={onSubmit}>
+        <MovieSelect />
+        <Stars />
+        <input name="comment"></input>
+        <button>전송</button>
+      </form>
+    </div>
   );
 }
 
